5608e3872faf3f35088cf9bf1b9b64f77d26df79
Danil Nikitin <d.nikitin.dev@gmail.com>
2023-02-14 18:41:26 +0300

    Refactoring. Updated data migration for Owner.flats.

--- property/migrations/0011_auto_20230213_1235.py
+++ property/migrations/0011_auto_20230213_1235.py
@@ -7,32 +7,24 @@ from django.db.backends.base.schema import BaseDatabaseSchemaEditor
 
 def add_flats_to_owner_model(apps: Apps, schema_editor: BaseDatabaseSchemaEditor) -> None:
     flat_owners = apps.get_model('property', 'Flat')
-    owner = apps.get_model('property', 'Owner')
+    owners = apps.get_model('property', 'Owner')
 
-    for owner in owner.objects.all():
-        owner_flats = flat_owners.objects.filter(
-            owner=owner.owner,
-            owners_phonenumber=owner.owners_phonenumber,
-            owner_pure_phone=owner.owner_pure_phone,
+    for owner in owners.objects.all().iterator():
+        owner.flats.set(flat_owners.objects.filter(
+                owner=owner.owner,
+                owners_phonenumber=owner.owners_phonenumber,
+                owner_pure_phone=owner.owner_pure_phone
+            )
         )
-        for owner_flat in owner_flats:
-            owner.flats.add(owner_flat)
-            owner.save()
+        owner.save()
 
 
 def move_backward(apps: Apps, schema_editor: BaseDatabaseSchemaEditor) -> None:
-    flat_owners = apps.get_model('property', 'Flat')
-    owner = apps.get_model('property', 'Owner')
+    owners = apps.get_model('property', 'Owner')
 
-    for owner in owner.objects.all():
-        owner_flats = flat_owners.objects.filter(
-            owner=owner.owner,
-            owners_phonenumber=owner.owners_phonenumber,
-            owner_pure_phone=owner.owner_pure_phone,
-        )
-        for owner_flat in owner_flats:
-            owner.flats.remove(owner_flat)
-            owner.save()
+    for owner in owners.objects.all().iterator():
+        owner.flats.clear()
+        owner.save()
 
 
 class Migration(migrations.Migration):
